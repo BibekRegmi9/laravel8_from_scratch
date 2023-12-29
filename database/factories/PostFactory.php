7f1d6e2d823f9f76e71bb59b5d328d6b2d8aed5c
Bibek <vivekregmi83@gmail.com>
2023-12-29 13:12:46 +0545

    Seed file is generated

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Category;
 
 class PostFactory extends Factory
 {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
+
+//        User::truncate();
+//        Post::truncate();
+//        Category::truncate();
+
+        $user = User::factory()->create([
+            'username' => 'Rami Malik'
+        ]);
+
+         Post::factory(10)->create([
+             'user_id' => $user->id
+         ]);
     }
 }
